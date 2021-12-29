b83c3b3e425214ce91d98dacd876d46aca1447ec
chinthakaw <chinthaka.wit@gmail.com>
2021-12-29 14:28:18 +0530

    set to all features 1

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,13 +1,10 @@
 package TestRunner;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 import qa.util.DeleteReportFiles;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
